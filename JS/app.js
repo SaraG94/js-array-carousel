7d90352f97d60a059b1dc185add9652073cfdd9b
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-06 16:30:04 +0100

    FEAT get element by html

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,14 @@
 console.log('Ciao')
 //prendere da html arrow
-//prendere photo da html
+const leftArrow = document.getElementsByClassName('arrow-left')
+console.log(leftArrow)
 
+const rightArrow = document.getElementsByClassName('arrow-right')
+console.log(rightArrow)
+
+//prendere photo da html
+const photoCarosello = document.getElementsByClassName('photo')
+console.log(photoCarosello)
 //ascoltare evento click su arrow
 
 // al click aggiungere e rimuovere classe active
