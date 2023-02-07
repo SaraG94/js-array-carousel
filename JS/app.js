4a38cc4da653c4374667befe50b210846fb95073
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-07 11:19:06 +0100

    ADD corretion milstone3

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,28 @@
+const slidesCarosello = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+]
+
+//recupere carosello 
+const carosello =document.getElementById('carosello')
+console.log(carosello)
+
+//ciclo img
+for (let i = 0; i < slidesCarosello.length; i++ ){
+    const img = slidesCarosello[i] 
+    console.log(img)
+    
+    //creo elemento html
+    const htmlPhoto=`
+    <div class="photo">
+        <img src="${img}" alt="">
+    </div>
+    `
+    carosello.innerHTML += htmlPhoto
+}
 
 //prendere da html arrow
 const leftArrow = document.getElementById('left')
@@ -12,6 +37,8 @@ console.log(photoCarosello)
 
 //indicephoto
 let indicePhotoAttiva = 0
+//alla photo con indice 0 aggiungere classe active
+photoCarosello[indicePhotoAttiva].classList.add('active')
 
 //ascoltare evento click su arrow
 
@@ -30,14 +57,12 @@ rightArrow.addEventListener('click', function () {
         let prossimaPhoto = photoCarosello[indicePhotoAttiva]
         // aggiungere classe active
         prossimaPhoto.classList.add('active')
-
     } 
 })
 
 
 leftArrow.addEventListener('click', function () {
 	console.log('ciaooo')
-
     
     if(indicePhotoAttiva>0){
         let photoCorrente = photoCarosello[indicePhotoAttiva]
@@ -50,7 +75,6 @@ leftArrow.addEventListener('click', function () {
         let previousPhoto = photoCarosello[indicePhotoAttiva]
         // aggiungere classe active
         previousPhoto.classList.add('active')
-
     }
 })
 
